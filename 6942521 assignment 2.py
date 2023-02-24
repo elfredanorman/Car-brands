94c501f36de035b7ef88a6d031267b7efa180150
elfredanorman <elfredan554@gmail.com>
2023-02-24 12:00:57 +0000

    Add files via upload

---
+++ 6942521 assignment 2.py
@@ -0,0 +1,59 @@
+#brand of cars
+carbrand = input("Please enter the car brand: ")
+if carbrand == "RANGE ROVER":
+ print('Yes and the price is GHC 385,000')
+elif carbrand == "HARMER":
+ print('Yes and the price is GHC 600,000')
+elif carbrand == "TOYOTA":
+ print('Yes and the price is GHC 500,000')
+elif carbrand == "HONDA":
+ print('Yes and the price is GHC 656,500')
+elif carbrand == "FORD":
+ print('Yes and the price is GHC 70,000')
+elif carbrand == "HYUNDAI":
+ print('Yes and the price is GHC 565,000')
+elif carbrand == "BENZ":
+ print('Yes and the price is GHC 450,000')
+elif carbrand == "LAMBORGHINI":
+ print('Yes and the price is GHC 785,400')
+elif carbrand == "TUNDRA":
+ print('Yes and the price is GHC 600,545')
+elif carbrand == "MITSHUBISHIN":
+ print('Yes and the price is GHC 690,000')
+elif carbrand == "TOYOTA FORTUNER":
+ print('Yes and the price is GHC 765,250')
+elif carbrand == "LAND CRUISER":
+ print('Yes and the price is GHC 141,990')
+elif carbrand == "NISSAN NAVARA":
+ print('Yes and the price is GHC 168,250')
+elif carbrand == "LEXUS RX350":
+ print('Yes and the price is GHC 270,000')
+elif carbrand == "TOYOTA RAV4":
+ print('Yes and the price is GHC 198,000')
+elif carbrand == "MERCEDES BENZ C-CLASS":
+ print('Yes and the price is GHC 300,000')
+elif carbrand == "HYUNDAI ELANTRA":
+ print('Yes and the price is GHC 84,000')
+elif carbrand == "KIA SPORTAGE":
+ print('Yes and the price is GHC 92,000')
+elif carbrand == "COROLLA":
+ print('Yes and the price is GHC 12,000')
+elif carbrand == "JAGUAR":
+ print('Yes and the price is GHC 150,000')
+elif carbrand == "TESLA":
+ print('Yes and the price is GHC 29,000')
+elif carbrand == "HONDA":
+ print('Yes and the price is GHC 90,000')
+elif carbrand == "PEUGOT":
+ print('Yes and the price is GHC 45,150')
+elif carbrand == "PORSCHE":
+ print('Yes and the price is GHC 35,000')
+elif carbrand == "MAZDA":
+ print('Yes and the price is GHC 62,000')
+elif carbrand == "NISSAN NAVARA":
+ print('Yes and the price is GHC 459,250')
+elif carbrand == "LEXUS LX 570":
+ print('Yes and the price is GHC 1,958,650')
+else:
+ print("no")
+#https://github.com/elfredanorman/Car-brands
